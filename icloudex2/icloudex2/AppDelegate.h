544b96f8a4c905333f5f651750f07fa6100be61f
Clovis Sangrail <clovis@systems-power.com>
2013-01-02 17:25:43 +0900

    only main class

---
+++ icloudex2/icloudex2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  icloudex2
+//
+//  Created by Jun Oura on 2013/01/02.
+//  Copyright (c) 2013年 Systems Power. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ icloudex2/icloudex2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  icloudex2
+//
+//  Created by Jun Oura on 2013/01/02.
+//  Copyright (c) 2013年 Systems Power. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
