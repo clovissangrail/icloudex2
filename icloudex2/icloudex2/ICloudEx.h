0ec7bbf8548ddc2f40e15cba2ba6ec6342b4872d
Clovis Sangrail <clovis@systems-power.com>
2013-01-02 17:32:26 +0900

    add main files

---
+++ icloudex2/icloudex2/ICloudEx.h
@@ -0,0 +1,13 @@
+//
+//  ICloudEx.h
+//  icloudex2
+//
+//  Created by Jun Oura on 2013/01/02.
+//  Copyright (c) 2013年 Systems Power. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ICloudEx : UIViewController
+
+@end
---
+++ icloudex2/icloudex2/ICloudEx.m
@@ -0,0 +1,38 @@
+//
+//  ICloudEx.m
+//  icloudex2
+//
+//  Created by Jun Oura on 2013/01/02.
+//  Copyright (c) 2013年 Systems Power. All rights reserved.
+//
+
+#import "ICloudEx.h"
+
+@interface ICloudEx ()
+
+@end
+
+@implementation ICloudEx
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
